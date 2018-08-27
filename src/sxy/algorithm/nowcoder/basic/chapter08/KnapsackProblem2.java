6ec56590a104237c1734a439bd88ffedf69b1c3a
Kevin Su <34428640+ycydsxy@users.noreply.github.com>
2018-08-27 09:55:47 +0800

    背包问题.
    Signed-off-by: Kevin Su <34428640+ycydsxy@users.noreply.github.com>

--- src/sxy/algorithm/nowcoder/basic/chapter08/KnapsackProblem2.java
+++ src/sxy/algorithm/nowcoder/basic/chapter08/KnapsackProblem2.java
@@ -3,21 +3,22 @@ package sxy.algorithm.nowcoder.basic.chapter08;
 import static java.lang.Math.*;
 
 /**
- * 0/1背包问题
+ * 完全背包问题
  * 
- * 给定两个数组w和v，两个数组长度相等，w[i]表示第i件商品的 重量，v[i]表示第i件商品的价值。 再给定一个整数bag，要求
+ * 给定两个数组w和v，两个数组长度相等，w[i]表示第i件商品的 重量，v[i]表示第i件商品的价值，每件物品有无数个。 再给定一个整数bag，要求
  * 你挑选商品的重量加起来一定不能超 过bag，返回满足这个条件 下，你能获得的最大价值
  * 
  * @author Kevin
  * 
  */
-public class KnapsackProblem1 {
+public class KnapsackProblem2 {
 	public static int maxValue1(int[] w, int[] v, int bag) {
 		return process(0, bag, w, v);
 	}
 
 	// 剩余背包重量为restWeight时，从index开始随意选择的最大价值，并返回
 	private static int process(int index, int restWeight, int[] w, int[] v) {
+
 		if (restWeight < 0) {
 			return Integer.MIN_VALUE;
 		}
@@ -26,23 +27,39 @@ public class KnapsackProblem1 {
 			return 0;
 		}
 
-		return max(process(index + 1, restWeight, w, v),
-				process(index + 1, restWeight - w[index], w, v) + v[index]);
+		int max = Integer.MIN_VALUE;
+		int i = 0;
+		while (true) {
+			if (restWeight - w[index] * i < 0) {
+				break;
+			}
+			max = max(max, process(index + 1, restWeight - w[index] * i, w, v)
+					+ v[index] * i);
+			i++;
+		}
+
+		return max;
 	}
 
 	public static int maxValue2(int[] w, int[] v, int bag) {
-		int[][] dp = new int[bag + 1][w.length];
-		for (int i = w[w.length - 1]; i <= bag; i++) {
-			dp[i][w.length - 1] = v[w.length - 1];
-		}
+		int[][] dp = new int[bag + 1][w.length + 1];
 
-		for (int j = w.length - 2; j >= 0; j--) {
-			for (int i = 1; i <= bag; i++) {
-				int temp = i - w[j] < 0 ? Integer.MIN_VALUE
-						: dp[i - w[j]][j + 1] + v[j];
-				dp[i][j] = max(dp[i][j + 1], temp);
+		for (int j = w.length - 1; j >= 0; j--) {
+			for (int i = 0; i <= bag; i++) {
+				int max = Integer.MIN_VALUE;
+				int k = 0;
+				while (true) {
+					if (i - w[j] * k < 0) {
+						break;
+					}
+					max = max(max, dp[i - w[j] * k][j + 1] + v[j] * k);
+					dp[i][j] = max;
+					k++;
+				}
+
 			}
 		}
+
 		return dp[bag][0];
 	}
 
---
+++ src/sxy/algorithm/nowcoder/basic/chapter08/KnapsackProblem3.java
@@ -0,0 +1,76 @@
+package sxy.algorithm.nowcoder.basic.chapter08;
+
+import static java.lang.Math.*;
+
+/**
+ * 多重背包问题
+ * 
+ * 给定两个数组w和v，两个数组长度相等，w[i]表示第i件商品的 重量，v[i]表示第i件商品的价值，c[i]表示第i件商品的数量。
+ * 再给定一个整数bag，要求 你挑选商品的重量加起来一定不能超 过bag，返回满足这个条件 下，你能获得的最大价值
+ * 
+ * @author Kevin
+ * 
+ */
+public class KnapsackProblem3 {
+	public static int maxValue1(int[] w, int[] v, int[] c, int bag) {
+		return process(0, bag, w, v, c);
+	}
+
+	// 剩余背包重量为restWeight时，从index开始随意选择的最大价值，并返回
+	private static int process(int index, int restWeight, int[] w, int[] v,
+			int[] c) {
+
+		if (restWeight < 0) {
+			return Integer.MIN_VALUE;
+		}
+
+		if (index == w.length) {
+			return 0;
+		}
+
+		int max = Integer.MIN_VALUE;
+		int k = 0;
+		while (true) {
+			if (restWeight - w[index] * k < 0 || k > c[index]) {
+				break;
+			}
+			max = max(max,
+					process(index + 1, restWeight - w[index] * k, w, v, c)
+							+ v[index] * k);
+			k++;
+		}
+
+		return max;
+	}
+
+	public static int maxValue2(int[] w, int[] v, int[] c, int bag) {
+		int[][] dp = new int[bag + 1][w.length + 1];
+
+		for (int j = w.length - 1; j >= 0; j--) {
+			for (int i = 0; i <= bag; i++) {
+				int max = Integer.MIN_VALUE;
+				int k = 0;
+				while (true) {
+					if (i - w[j] * k < 0 || k > c[j]) {
+						break;
+					}
+					max = max(max, dp[i - w[j] * k][j + 1] + v[j] * k);
+					dp[i][j] = max;
+					k++;
+				}
+
+			}
+		}
+
+		return dp[bag][0];
+	}
+
+	public static void main(String[] args) {
+		int[] w = { 3, 2, 4, 7 };
+		int[] v = { 5, 6, 3, 19 };
+		int[] c = { 2, 0, 3, 4 };
+		int bag = 11;
+		System.out.println(maxValue1(w, v, c, bag));
+		System.out.println(maxValue2(w, v, c, bag));
+	}
+}
