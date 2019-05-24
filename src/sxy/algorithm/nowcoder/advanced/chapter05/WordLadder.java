0b20e8fbeaed0c3c41a83aa48d1d8fe3ec06db2e
Kevin Su <34428640+ycydsxy@users.noreply.github.com>
2019-05-24 16:29:26 +0800

    WordLadder.
    Signed-off-by: Kevin Su <34428640+ycydsxy@users.noreply.github.com>

---
+++ src/sxy/algorithm/nowcoder/advanced/chapter05/WordLadder.java
@@ -0,0 +1,52 @@
+package sxy.algorithm.nowcoder.advanced.chapter05;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * 给两个单词beginWord和endWord，并给一个单词集wordList，希望从beginWord变成endWord，每次变一个字母，
+ * 且每次变完的结果都在wordList中.问从beginWord变到endWord最少的步数是多少?
+ * 
+ * @author Kevin
+ * 
+ */
+public class WordLadder {
+
+	public static List<List<String>> findLadders(String beginWord,
+			String endWord, List<String> wordList) {
+		wordList.add(beginWord);
+		// 生成邻居表
+		HashMap<String, ArrayList<String>> nexts = getNexts(wordList);
+		// 从start开始，进行宽度优先遍历，求每一个字符串到start的最短距离
+		HashMap<String, Integer> distances = getDistances(beginWord, nexts);
+		// 深度优先遍历收集答案
+		LinkedList<String> pathList = new LinkedList<>();
+		List<List<String>> res = new ArrayList<>();
+		getShortestPaths(beginWord, endWord, nexts, distances, pathList, res);
+		return res;
+
+	}
+
+	private static void getShortestPaths(String beginWord, String endWord,
+			HashMap<String, ArrayList<String>> nexts,
+			HashMap<String, Integer> distances, LinkedList<String> pathList,
+			List<List<String>> res) {
+		// TODO Auto-generated method stub
+
+	}
+
+	private static HashMap<String, Integer> getDistances(String beginWord,
+			HashMap<String, ArrayList<String>> nexts) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	private static HashMap<String, ArrayList<String>> getNexts(
+			List<String> wordList) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
